d2a387fac42252baaf87199b598eade21e928a10
Miro Hrončok <miro@hroncok.cz>
2013-07-08 22:01:04 +0200

    Can list files and dirs (both as files)

---
+++ dbox-fuse.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+import os, stat, errno, locale
+import fuse
+from fuse import Fuse
+from dropbox import client, rest, session
+
+if not hasattr(fuse, '__version__'):
+    raise RuntimeError, \
+        "your fuse-py doesn't know of fuse.__version__, probably it's too old."
+
+fuse.fuse_python_api = (0, 2)
+
+version = "0.0.1"
+
+from _login import *
+#APP_KEY = 'foo'
+#APP_SECRET = 'bar'
+
+TOKEN_FILE = os.path.expanduser('~/.dbox-fuse-token')
+
+class StoredSession(session.DropboxSession):
+    """a wrapper around DropboxSession that stores a token to a file on disk"""
+    def load_creds(self):
+        try:
+            stored_creds = open(TOKEN_FILE).read()
+            self.set_token(*stored_creds.split('|'))
+            print "[loaded access token]"
+        except IOError:
+            pass # don't worry if it's not there
+
+    def write_creds(self, token):
+        f = open(TOKEN_FILE, 'w')
+        f.write("|".join([token.key, token.secret]))
+        f.close()
+
+    def delete_creds(self):
+        os.unlink(TOKEN_FILE)
+
+    def link(self):
+        request_token = self.obtain_request_token()
+        url = self.build_authorize_url(request_token)
+        print("url: "+url)
+        print("Please authorize in the browser. After you're done, press enter.")
+        raw_input()
+
+        self.obtain_access_token(request_token)
+        self.write_creds(self.token)
+
+    def unlink(self):
+        self.delete_creds()
+        session.DropboxSession.unlink(self)
+
+class MyStat(fuse.Stat):
+    def __init__(self):
+        self.st_mode = 0
+        self.st_ino = 0
+        self.st_dev = 0
+        self.st_nlink = 0
+        self.st_uid = 0
+        self.st_gid = 0
+        self.st_size = 0
+        self.st_atime = 0
+        self.st_mtime = 0
+        self.st_ctime = 0
+
+class DboxFuse(Fuse):
+    def __init__(self, *args, **kw):
+        Fuse.__init__(self, *args, **kw)
+        self.sess = StoredSession(APP_KEY, APP_SECRET, access_type='dropbox')
+        self.api_client = client.DropboxClient(self.sess)
+        self.sess.load_creds()
+        if not self.sess.is_linked():
+            try:
+                self.sess.link()
+            except rest.ErrorResponse, e:
+                print('Error: '+e)
+                exit(1)
+
+    def getattr(self, path):
+        st = MyStat()
+        if path == '/':
+            st.st_mode = stat.S_IFDIR | 0755
+            st.st_nlink = 2
+        else:
+            st.st_mode = stat.S_IFREG | 0444
+            st.st_nlink = 1
+            st.st_size = 10
+        return st
+
+    def readdir(self, path, offset):
+        resp = self.api_client.metadata(path)
+        if 'contents' in resp:
+            for f in resp['contents'] + [{'path': '.'}, {'path': '..'}]:
+                name = os.path.basename(f['path'])
+                encoding = locale.getdefaultlocale()[1]
+                yield fuse.Direntry(name.encode(encoding))
+
+    def open(self, path, flags):
+        return -errno.EACCES
+
+    def read(self, path, size, offset):
+        return -errno.EACCES
+
+if __name__ == '__main__':
+    usage="""
+Dropbox fuse mount
+
+""" + Fuse.fusage
+    try:
+        server = DboxFuse(version="%prog " + version,
+                         usage=usage,
+                         dash_s_do='setsingle')
+
+        server.parse(errex=1)
+        server.main()
+    except fuse.FuseError:
+        pass
