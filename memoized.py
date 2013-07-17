ff3ce6cc482c6b704fd0f48aa2340799f2bf4f67
Miro Hrončok <miro@hroncok.cz>
2013-07-17 14:42:09 +0200

    Actually add memoized.py and not .pyc

---
+++ memoized.py
@@ -0,0 +1,30 @@
+import collections
+import functools
+from datetime import datetime, timedelta
+from time import sleep
+
+class memoized(object):
+   '''Decorator. Caches a function's return value each time it is called.
+   If called later with the same arguments, the cached value is returned
+   (not reevaluated), unless more than 5 seconds passed.
+   '''
+   def __init__(self, func):
+      self.func = func
+      self.cache = {}
+   def __call__(self, *args):
+      if not isinstance(args, collections.Hashable):
+         # uncacheable. a list, for instance.
+         # better to not cache than blow up.
+         return self.func(*args)
+      if args in self.cache and self.cache[args][0] >= datetime.now()-timedelta(seconds=5):
+         return self.cache[args][1]
+      else:
+         value = self.func(*args)
+         self.cache[args] = datetime.now(), value
+         return value
+   def __repr__(self):
+      '''Return the function's docstring.'''
+      return self.func.__doc__
+   def __get__(self, obj, objtype):
+      '''Support instance methods.'''
+      return functools.partial(self.__call__, obj)
